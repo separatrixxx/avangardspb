79c782d568e4fcffc5319c298ba6db63ae1d3a1e
separatrixxx <nikitalochmatov@gmail.com>
2023-12-27 20:29:12 +0300

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,6 +22,5 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <Component {...pageProps} />
     </>
-
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,26 @@
 import { MainPage } from "../../page_components/MainPage/MainPage";
+import Head from 'next/head';
+import { useRouter } from "next/router";
+import { setLocale } from "../../helpers/locale.helper";
 
 
 function Main(): JSX.Element {
-    return (
+  const router = useRouter();
+
+  return (
+    <>
+      <Head>
+        <title>{setLocale(router.locale).avangard + ' - ' + setLocale(router.locale).slogan}</title>
+        <meta name='description' content={setLocale(router.locale).avangard + ' - ' + setLocale(router.locale).slogan} />
+        <meta property='og:title' content={setLocale(router.locale).avangard + ' - ' + setLocale(router.locale).slogan} />
+        <meta property='og:description' content={setLocale(router.locale).avangard + ' - ' + setLocale(router.locale).slogan} />
+        <meta charSet="utf-8" />
+        <meta name="yandex-verification" content="5344a215c217462e" />
+        <link rel="icon" href="/avangard.png" type='image/png' />
+      </Head>
       <MainPage />
-    );
+    </>
+  );
 }
 
 export default Main;
